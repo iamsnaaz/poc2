7b685ec5af30e954e2a80548ff809b0e1b829013
iamsnaaz <sadiyanaaz4255@gmail.com>
2026-04-07 13:09:29 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,11 +9,41 @@ public class App {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         server.createContext("/", exchange -> {
-            String response = "Hello from CI/CD PoC 🚀";
-            exchange.sendResponseHeaders(200, response.length());
+            String response = """
+                <html>
+                <head>
+                    <title>CI/CD POC</title>
+                    <style>
+                        body {
+                            font-family: Arial;
+                            text-align: center;
+                            background-color: #f4f4f4;
+                            margin-top: 50px;
+                        }
+                        h1 {
+                            color: #2c3e50;
+                        }
+                        p {
+                            font-size: 18px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h1>🚀 CI/CD POC Successful!</h1>
+                    <p>Java app deployed on EKS</p>
+                    <p>Using Terraform + GitHub Actions</p>
+                </body>
+                </html>
+            """;
+
+            exchange.getResponseHeaders().add("Content-Type", "text/html");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
+
+            System.out.println("Served HTML page");
         });
 
         server.start();
